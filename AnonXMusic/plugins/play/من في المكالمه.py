1cce7b694feddfb7c78a173e4fe1939e9c04302c
Source-Ze1 <146869174+Source-Ze1@users.noreply.github.com>
2023-11-05 14:40:39 +0200

    Update من في المكالمه.py
    Signed-off-by: Source-Ze1 <146869174+Source-Ze1@users.noreply.github.com>

--- AnonXMusic/plugins/play/من في المكالمه.py
+++ AnonXMusic/plugins/play/من في المكالمه.py
@@ -4,7 +4,7 @@ import asyncio
 from pyrogram.types import VideoChatEnded, Message
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from AnonXMusic.core.call import Anon
+from AnonXMusic.core.call import Anony
 from AnonXMusic.utils.database import *
 from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJoinedError)
 
