d7bb3fb8e35be94a594ef749460e56ae2c51bb0a
Tm-AbdoX <tmabdox@gmail.com>
2024-08-31 19:47:59 +0300

    Update config.py
    Signed-off-by: Tm-AbdoX  <tmabdox@gmail.com>

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Source-Ze1/ZE-MUSIC",
+    "https://github.com/TmAbdoX/ZE-MUSIC.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
